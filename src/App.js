6ecbcec97b838823b4061acaf03ba4631a8f9961
Activx-nV <68726573+Activx-nV@users.noreply.github.com>
2021-09-18 11:51:55 +0300

    Create app skeleton (#1)
    * feat: create an app skeleton
    
    * refactor: wrap tests in a BrowserRouter component
    
    * feat: add custom favicon + refactor: global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Header from './components/Header';
+
+const AppWrapper = styled.div`
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  background: #ffffff;
+  font-size: 16px;
+  font-style: normal;
+  font-family: Montserrat, sans-serif;
+  font-weight: normal;
+  line-height: 1.69;
+  color: #93918f;
+`;
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <AppWrapper>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            Home
+          </Route>
+          <Route path="/search">Search</Route>
+          <Route>404 - Not Found</Route>
+        </Switch>
+      </AppWrapper>
+    </Router>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import classes from './Header.module.css';
+import logo from '../assets/logo.svg';
+
+const Header = () => (
+  <header className={classes.header}>
+    <Link to="/">
+      <img src={logo} className={classes.logo} alt="reddit logo" />
+    </Link>
+    <nav className={classes.nav}>
+      <ul className={classes.list}>
+        <li className={classes.item}>
+          <NavLink className={classes.item_link} to="/search">
+            Search
+          </NavLink>
+        </li>
+        <li className={classes.item}><a href="#how-it-works" className={classes.item_link}>How it works</a></li>
+        <li className={classes.item}><a href="#about" className={classes.item_link}>About</a></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
