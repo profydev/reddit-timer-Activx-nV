71b96780aa5ee1d1a6c44e31783dbd44a77ac22f
Activx-nV <68726573+Activx-nV@users.noreply.github.com>
2021-09-19 12:17:53 +0300

    #2 Add Header
    * feat: add header
    
    * refactor: add routing in app.js
    
    * refactor: header styles, change <a> tags to navlink

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,20 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('Header', () => {
+  test('"How it works" link points to the correct page', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const link = screen.getByRole('link', { name: /how it works/i });
+    userEvent.click(link);
+  });
+});
